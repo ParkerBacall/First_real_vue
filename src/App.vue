e9397609985c6ec54166d7efb9fd90b3c44d578d
ParkerBacall <pbacall2@gmail.com>
2020-02-23 21:07:42 -0700

    post new todos

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <AddTodo v-on:add-todo="createTodo"/>
     <TodoList v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
     import TodoList from './components/TodoList'; 
+    import AddTodo from './components/AddTodo'; 
 export default {
   name: 'App',
   components: {
-    TodoList
+    TodoList,
+    AddTodo
   },
   data() {
     return {
@@ -28,8 +31,20 @@ export default {
       fetch(`http://localhost:8001/todos/${id}`, {
         method: 'DELETE'
       })
+    },
+    createTodo(newTodo){
+      console.log(newTodo)
+      this.todos = [...this.todos, newTodo]
+      fetch('http://localhost:8001/todos/', {
+         method: 'POST',
+         headers:{
+           'content-type': 'application/json'
+         },
+         body: JSON.stringify(newTodo)
+      })
     }
   },
+
     mounted(){
       this.fetchTodos()
     }
--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,5 +1,38 @@
 <template>
-    <div>
-        
+    <div  id='add-todo-form'>
+        <form @submit="addTodo">
+            <input type="text" v-model="title" name="title" placeholder="todo title">
+            <input type="text" v-model="text" name="text" placeholder="todo text">
+            <input type="submit">
+        </form>
     </div>
 </template>
+
+<script>
+export default {
+    name: "AddTodo",
+    data() {
+        return{
+            title: '',
+            text: ''
+        }
+    },
+        methods: {
+            addTodo(e){
+                e.preventDefault()
+                const newTodo ={ 
+                    title: this.title,
+                    text: this.text
+                }
+            this.$emit('add-todo', newTodo)
+            }
+        }
+}
+</script>
+
+<style lang="css" scoped>
+    #add-todo-form{
+        display: flex;
+        justify-content: center;
+    }
+</style>
